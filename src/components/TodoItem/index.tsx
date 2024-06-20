2ff21955922c088bc84f639ffbfb9d254c74c4e0
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2024-06-20 11:07:12 -0300

    feat(Home): add base structure and styles

---
+++ src/components/TodoItem/index.tsx
@@ -0,0 +1,53 @@
+import { CheckIcon } from "@/components/Icons/CheckIcon";
+
+interface TodoItemProps {}
+
+export function TodoItem({}: TodoItemProps) {
+  return (
+    <div className="flex bg-green-950 w-full text-white rounded-sm p-5">
+      <div className="flex-1">
+        <div className="mb-4">
+          <p className="text-lg flex items-center">
+            <button className="size-4 mr-2 fill-white">
+              <CheckIcon />
+            </button>
+            Postes de luz que ligam automático na vila
+          </p>
+
+          <span className="text-green-400">* Criada por &quot;username&quot;</span>
+        </div>
+
+        <ul className="flex gap-4">
+          {['Construção', 'Automação'].map((categoryName) => (
+            <li key={categoryName} className="bg-green-400 rounded-sm p-1">
+              # {categoryName}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="flex flex-col justify-between">
+        <strong className="text-base">Estão ajudando:</strong>
+
+        <ul className="flex justify-end">
+          <li>
+            <button className="bg-green-400 size-9 text-3xl leading-none flex items-center justify-center z-20 relative shadow-[2px_0_3.5px_rgb(0_0_0_/_50%)]">
+              +
+            </button>
+          </li>
+          {['lreporta', 'VenomExtreme', 'Monerk'].map((minecraftUsername, index) => (
+            <li key={minecraftUsername} className="-ml-1">
+              <img
+                className="size-9 relative shadow-[2px_0_3.5px_rgb(0_0_0_/_50%)]"
+                style={{ zIndex: 10 - index }}
+                src={`https://mineskin.eu/helm/${minecraftUsername}`}
+                alt={`Avatar ${minecraftUsername}`}
+                title={minecraftUsername}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
