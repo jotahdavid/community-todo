705486e81715c688eac9d4087ed85140d3b8e681
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2024-06-21 19:19:37 -0300

    feat: add force-dynamic on tasks route

--- src/app/tasks/route.ts
+++ src/app/tasks/route.ts
@@ -1,5 +1,7 @@
 import TaskRepository from '@/repositories/TaskRepository';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   const tasks = await TaskRepository.getAll();
   return Response.json(tasks);
