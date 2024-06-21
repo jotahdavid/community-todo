134c4f9985ce1b68600fe3edc98af119b3e1013f
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2024-06-21 19:17:24 -0300

    fix: no store cache on fetch

--- src/services/tasksService/getAll.ts
+++ src/services/tasksService/getAll.ts
@@ -2,7 +2,7 @@ import { Task } from '@/repositories/TaskRepository';
 
 
 export async function getAll(): Promise<Task[]> {
-  const response = await fetch('/tasks');
+  const response = await fetch('/tasks', { cache: 'no-store' });
   const data = response.json();
   return data;
 }
